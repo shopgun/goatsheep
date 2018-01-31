60198fb5b796a23863801130a619e4cf8e948743
Iasonas Zotos <iz@shopgun.com>
2018-01-31 20:05:08 +0100

    basic structure, centroids, payload validation

--- test/goatship.js
+++ test/goatship.js
@@ -3,5 +3,23 @@
  * - Make sure all functions are exposed in module.exports
  * - Input validation
  */
-const goatsheep = require('../.');
+const assert = require('assert');
+const util = require('util');
+const Goatsheep = require('../.');
+const input = require('./data/input_a');
 
+describe('Goatsheep', () => {
+    describe('Exports', () => {
+        it('should have all exports');
+    });
+
+    describe('Payload', () => {
+        it('should not throw for valid input', () => {
+            try {
+                new Goatsheep(input);
+            } catch (e) {
+                console.error(e.stack);
+            }
+        });
+    });
+});
